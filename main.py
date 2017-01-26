0d6967d8fdff0df472b6ab67d5a3c72c2423c5c2
Rasoulipour <rasoulipourmohammad@gmail.com>
2017-01-26 17:38:45 -0600

    the bugs fixed on escaping

--- main.py
+++ main.py
@@ -16,15 +16,37 @@
 #
 import webapp2
 import ceasar
+import cgi
+
+
+def page_builder(in_message):
+    rotation_label = '<label>rotate by:</label>'
+    message_label = '<label>message you want to encript:</label>'
+    rotation_input = '<input type = "number" name = "rotation">'
+    submit = '<input type="submit" />'
+    textarea = '<textarea name="message">'+ in_message +'</textarea>'
+
+    header = '<h1>Web Ceasar!</h1>'
+
+    form = ('<form method="post">' + message_label
+    + textarea + '<br>' + rotation_label + rotation_input
+    + '<br>' + submit + '</form>')
+
+    return header + form
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        message = 'kamsmas'
-        incripted_message = ceasar.encrypt(message, 12)
-        submit = '<input type="submit" />'
-        textarea = '<textarea>' + incripted_message + '</textarea>'
-        form = '<form>' + textarea + '<br>' + submit + '</form>'
-        self.response.write(form)
+        content = page_builder("")
+        self.response.write(content)
+
+    def post(self):
+        message = self.request.get("message")
+        rotation = int(self.request.get("rotation"))
+        incripted_message = ceasar.encrypt(message, rotation)
+        escaped_message = cgi.escape(incripted_message)
+        content = page_builder(incripted_message)
+        self.response.write(content)
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
